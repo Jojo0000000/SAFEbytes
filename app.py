0bad1abf861bfc7dc91a7f2b010ada4f508eaddd
Jyoti Bisht <bishtjyoti664@gmail.com>
2026-06-15 23:09:15 +0530

    Add Render deployment support with requirements and dynamic port

--- app.py
+++ app.py
@@ -126,4 +126,6 @@ def decrypt():
     return jsonify({'decrypted_text': decrypted_text})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
